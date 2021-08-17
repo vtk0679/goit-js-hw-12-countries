f36e252ceed7ba8470cf5f369e5c763a4c76ff25
Vitaliy <vtk0679@gmail.com>
2021-08-17 15:49:41 +0300

    prettifies code in index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const debounce = require('lodash.debounce');
 refs.input.oninput = debounce(onInput, 500);
 
 function onInput() {
-  fetchCountries(refs.input.value).then(data => {
-    if (data) {
-      if (data.length > 10) showError('Too match results');
-      else if (data.length > 2) render.CountriesList(data);
-      else render.Card(data[0]);
-    } else showError('Nothing found');
-  });
+  const request = refs.input.value;
+  if (request.length > 1)
+    fetchCountries(request).then(data => {
+      if (data) {
+        if (data.length === 1) render.Card(data[0]);
+        else if (data.length >= 2 && data.length <= 10) render.CountriesList(data);
+        else showError('Too match results');
+      } else showError('Nothing found');
+    });
 }
